2e382539b38eb4fa5f52db28c45728b1dbd8188c
fiarr4ik <fiarr4ik@gmail.com>
2025-02-27 12:28:56 +0700

    Сделал окна регистрации и авторизации

---
+++ kurs/Windows/AuthWindow.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System.Windows;
+
+namespace kurs.Windows
+{
+    public partial class AuthWindow : Window
+    {
+        public AuthWindow()
+        {
+            InitializeComponent();
+        }
+
+        public void OpenWindow(Window window)
+        {
+            window.Show();
+            this.Hide();
+        }
+
+        private void LoginButton_OnClick(object sender, RoutedEventArgs e)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        private void RegisterButton_OnClick(object sender, RoutedEventArgs e)
+        {
+            OpenWindow(new RegisterWindow());
+        }
+    }
+}
---
+++ kurs/Windows/RegisterWindow.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System.Windows;
+
+namespace kurs.Windows
+{
+    public partial class RegisterWindow : Window
+    {
+        public RegisterWindow()
+        {
+            InitializeComponent();
+        }
+
+        public void OpenWindow(Window window)
+        {
+            window.Show();
+            this.Hide();
+        }
+        
+        private void LoginButton_OnClick(object sender, RoutedEventArgs e)
+        {
+            OpenWindow(new AuthWindow());
+        }
+
+        private void RegisterButton_OnClick(object sender, RoutedEventArgs e)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
